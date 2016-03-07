4cbf67604c94c8e607e566b256dc003f4498ac95
David Zimmer <david.zimmer@pinnacol.com>
2016-03-07 10:43:27 -0700

    version 2 using jwt token.

--- app/js/controllers/ContactController.js
+++ app/js/controllers/ContactController.js
@@ -11,28 +11,38 @@ homeProjectApp.controller('ContactController', function ContactController($scope
         last_name: 'Zimmer',
         cell_number: '555-555-5555',
         email: 'canuckzed@gmail.com',
-        created: '2016-02-29'
+        created: '2016-02-29',
+        policy_number: '3232325',
+        session_id: '99999999'
     }
 
     var self = this;
+    var url = 'http://localhost:8811/token/' + $scope.contact.policy_number + '/' + $scope.contact.session_id;
+    console.log('tokenurl = ' + url);
 
+    //$http.get('http://localhost:8811/token/' + )
 
-    $scope.sendIt = function() {
-        console.log("In send it function...");
+    $http.get(url)
+        .then(function(response) {
+            console.log(response);
+            $scope.contact.token = response.data.token;
 
-        console.log('User entered for pol num = ' + $scope.contact.policy_number);
+            console.log('contact token ', $scope.contact.token);
+        })
 
-        var config = {headers: {
-            "X-TestingPol" : $scope.contact.policy_number,
-            "Access-Control-Allow-Origin": true
-        }}
-
-        $http.get('http://localhost:8088/#/view', config)
-            .then(function(response) {
-                console.log(response);
-            });
-
-
-    }
+    //$scope.sendIt = function() {
+    //    console.log("In send it function...");
+    //
+    //    console.log('User entered for pol num = ' + $scope.contact.policy_number);
+    //
+    //
+    //
+    //    $http.get('http://localhost:8088/#/view', config)
+    //        .then(function(response) {
+    //            console.log(response);
+    //        });
+    //
+    //
+    //}
 
 });
